fc2b291b32fb72fd6682685ebff68e61f9d54b61
bullert <b.bullert5@gmail.com>
2021-08-26 04:54:24 +0200

    Add account controller, add model, view & action for login & register

---
+++ BasicAuthenticationDemo/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using BasicAuthenticationDemo.Models;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthenticationDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> logger;
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(
+            ILogger<AccountController> logger,
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            this.logger = logger;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LoginAsync(LoginViewModel model)
+        {
+            if (!string.IsNullOrEmpty(model.UserName) &&
+                !string.IsNullOrEmpty(model.Password))
+            {
+                var user = await userManager.FindByNameAsync(model.UserName) ??
+                       await userManager.FindByEmailAsync(model.UserName);
+
+                if (user != null)
+                {
+                    var result = await signInManager.PasswordSignInAsync(
+                            user,
+                            model.Password,
+                            model.RememberMe,
+                            false
+                        );
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid username or password");
+
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterAsync(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
