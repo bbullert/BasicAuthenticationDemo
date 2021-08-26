d05692df8ee466dc85a2b73e3728cd776998cb30
bullert <b.bullert5@gmail.com>
2021-08-26 02:35:17 +0200

    Setup data access project

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ DataAccess/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class AppUser : IdentityUser
+    {
+    }
+}
